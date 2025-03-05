59ef45581b953b0e3769996498ed4242809ab712
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-03-05 14:26:49 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4'>Hi World!</h1>
+      <p>Muhammad Jawwad Khan Sherwani</p>
+    </>
+  )
+}
+
+export default App
